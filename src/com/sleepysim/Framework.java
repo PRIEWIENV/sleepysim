e0d49cdfe373a35edf498182b3baea51d7af73bb
niconiconi <niconiconi@sjtu.edu.cn>
2017-07-06 11:22:35 -0400

    add comment

--- src/com/sleepysim/Framework.java
+++ src/com/sleepysim/Framework.java
@@ -4,49 +4,34 @@ import java.util.ArrayList;
 
 public class Framework
 {
-    private Adversary adversary;
     private ArrayList <ArrayList <Message>> message_wait_for_send;
     private ArrayList <ArrayList <Message_to_send>> message_buffer;
     private ArrayList <Message_to_send> message_to_corrupted_buffer;
-    private Integer node_count, adversary_count;
-    private Signature signature;
-    private Node node_list[];
+    private ArrayList <Node> node_list;
     private Integer message_count;
     private Integer delay;
     private Integer current_round;
 
-    Framework(Integer node_count, Integer adversary_count, Signature signature, Adversary adversary, Integer delay)
+    Framework(Integer delay, ArrayList <Node> node_list)
     {
         //some code goes here
         //For framework team
+        int n = node_list.size();
         message_wait_for_send = new ArrayList<>();
-        for (int i = 0; i < node_count; i ++)
+        for (int i = 0; i < n; i ++)
             message_wait_for_send.add(new ArrayList<>());
         message_buffer = new ArrayList<>();
-        for (int i = 0; i < node_count; i ++)
+        for (int i = 0; i < n; i ++)
             message_buffer.add(new ArrayList<>());
-        this.node_count = node_count;
-        this.adversary_count = adversary_count;
-        this.signature = signature;
-        this.adversary = adversary;
-        node_list = new Node[node_count];
-        for (int i = 0; i < adversary_count; i ++)
-            node_list[i] = new Corrupted_node(i, adversary);
-        for (int i = adversary_count; i < node_count; i ++)
-            node_list[i] = new Honest_node(i);
         message_count = 0;
         this.delay = delay;
+        this.node_list = node_list;
         current_round = 0;
     }
 
     void next_round()
     {
         current_round ++;
-    }
-
-    void run_node(int node_id)
-    {
-        node_list[node_id].run();
     }
 
     ArrayList <Message> send_message(Integer target)
