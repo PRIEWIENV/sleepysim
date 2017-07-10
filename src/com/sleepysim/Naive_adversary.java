3c39e85c43c6986314e376bf0af36a185f0c9975
陈清荣 <chenqingrong@dhcp-rhodes-678.wifi-visitor.cit.cornell.edu>
2017-07-10 12:20:25 -0400

    add elements

--- src/com/sleepysim/Naive_adversary.java
+++ src/com/sleepysim/Naive_adversary.java
@@ -13,18 +13,21 @@ public class Naive_adversary implements Adversary
     private ArrayList<PublicKey> public_key_table;
     private Chain chain;
     private ArrayList<Block> latest_blocks;
+    private ArrayList<Node> corrupt_nodes;
     /**
      * A naive adversary, you should break consistency if you have more node than honest
      * @param n number of corrupted nodes
      * @param secret_key_table secret key for each corrupted node, the Integer is node id, and String is the corresponding secret key
      * @param public_key_table public keys
      */
-    public Naive_adversary(Integer n, ArrayList<Pair<Integer, PrivateKey>> secret_key_table, ArrayList<PublicKey> public_key_table, Block genesis)
+    public Naive_adversary(Integer n, ArrayList<Pair<Integer, PrivateKey>> secret_key_table, ArrayList<PublicKey> public_key_table,ArrayList<Node> corrupt, Block genesis)
     {
         this.n = n;
         this.secret_key_table = secret_key_table;
         this.public_key_table = public_key_table;
+        this.corrupt_nodes=corrupt;
         chain.chain.put(genesis.get_current_hash(),genesis);
+        latest_blocks.add(genesis);
     }
 
     /**
